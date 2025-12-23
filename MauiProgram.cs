6a47a2162e776ec08e32057ce8bec3d1c0be788a
miyazawa <hayatomz@gmail.com>
2025-12-23 22:23:22 +0900

    Implement Convert tab: D&D integration, multi-engine selection, folder picker

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using MarkBridge.Services;
 using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace MarkBridge;
 
@@ -10,6 +11,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
